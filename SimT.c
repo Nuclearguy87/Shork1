e2e06f1d3fdc2d2761dac63e0f06dfdbf65830e9
Nuclearguy87 <77165080+Nuclearguy87@users.noreply.github.com>
2023-06-14 08:01:08 +0000

    Add files via upload
    fukin segfaults

--- SimT.c
+++ SimT.c
@@ -13,16 +13,22 @@ int main(){
 int monitor(int * p_RAM)
 {
     char chr[10];
+    int numb = 0;
     scanf("%s",&chr,sizeof(chr));     
 
-    int i;
-    while (chr[i] >= 48 && chr[i] <= 57 && chr[i] >= 65 && chr[i] <= 70){
-    
+    int i = 3;
+    int e = 1;
+    while ((chr[i] >= 48 && chr[i] <= 57) || (chr[i] >= 65 && chr[i] <= 70)){
+	    numb += chr[i]-48;
+	    e *= 10;
+	    i++;
+	    printf(i);
     }
+    i = 0;
 
     switch(chr[0]){
 	case 'p': if(chr[1] == 'k'){
-		printf("%d",*(p_RAM+1));
+		printf("%d",numb);
 		}		
 	 break;
 	case 'j': break;
